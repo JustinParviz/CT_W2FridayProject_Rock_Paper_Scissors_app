c0fbdea5b265ed919fa6a88580aec6741e79f1c8
JustinParviz <jarshravan@gmail.com>
2024-01-16 08:15:59 -0500

    Some minor updates

--- Rock_Paper_Scissors.py
+++ Rock_Paper_Scissors.py
@@ -5,8 +5,8 @@ def rock_paper_scissors():
     response = input("Ready to get dominated by the computer in rock, paper, scissors? Yes or No? ").lower()
     if response == "yes":
         while True:
-            computer_input = random.choice(["rock","paper" , "scissors"])
-            user_input = input("Would you like to choose rock, paper, or scissors? ")
+            computer_input = random.choice(["rock", "paper" , "scissors"])
+            user_input = input("Would you like to choose rock, paper, or scissors? ").lower()
             if user_input == "rock":
                 if computer_input == "rock":
                     print("Congratulations, you've reached the digital paradox! It seems our binary buddy, rock, is having a bit of a tiebreaker. Meanwhile, scissors is just sharpening its blades for the next showdown. Try again to resolve this epic duel.")
@@ -42,7 +42,7 @@ def rock_paper_scissors():
                 elif computer_input == "paper":
                     print("You're a Winner Winner CHICKEN DINNER!!!")
                 elif computer_input == "scissors":
-                    print("Alert: Quantum entanglement detected in the Rock, Paper, Scissors matrix. It seems the universe can't decide either. Please stand by while we attempt to debug reality and figure out if we're dealing with a glitch in the matrix or just an epic showdown of eternal indecision.")
+                    print("Alert: A Quantum Entanglement has been detected in the Rock, Paper, Scissors matrix. It seems the universe can't decide either. Please stand by while we attempt to debug reality and figure out if we're dealing with a glitch in the matrix or just an epic showdown of eternal indecision.")
             if user_input == "quit":
                 print("You have quit")
                 break
